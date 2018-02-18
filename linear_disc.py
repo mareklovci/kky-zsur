9f8db95e135b98730ef02bd07693c3a3ba96b247
mareklovci <mareklovci@gmail.com>
2018-02-18 21:42:36 +0100

    Prepared files
    Just some prepared files for further development

---
+++ linear_disc.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""linear_disc.py: Klasifikator s linearnimi diskriminacnimi funkcemi
+
+* Rossenblattuv algoritmus
+* metoda konstantnich prirustku
+* upravena metoda konstantnich prirustku"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ nearest_neighbour.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+"""nearest_neighbour.py: Klasifikator podle nejblizsiho a dvou nejblizsich sousedu"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
