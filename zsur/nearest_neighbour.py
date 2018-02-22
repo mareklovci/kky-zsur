ddeee66ea10cf72ed5093f7b4a23b4137c6d15c5
mareklovci <mareklovci@gmail.com>
2018-02-22 13:30:12 +0100

    Implemented simple kNN classifier
    Implemented simple version of kNN classifier, but I am not sure if it's correct. It works only for k = 2. I will improve it later, maybe...

--- zsur/nearest_neighbour.py
+++ zsur/nearest_neighbour.py
@@ -22,10 +22,26 @@ def nearest_neighbour(data, classes, space_size=(-20, 20), step=1):
     plot_kmeans(kmeans_toplot)
 
 
+def knearest_neighbour(data, classes, space_size=(-20, 20), step=1):
+    kmeans = k_means(data, classes)
+    trypoints = generate_points(space_size[0], space_size[1], step)
+    means_toplot = dict(kmeans)
+    for trypoint in trypoints:
+        for val in kmeans.values():
+            val.sort(key=lambda p: distanc(trypoint, p))
+        newdict = {}
+        for key, val in kmeans.items():
+            newdict[key] = (distanc(kmeans[key][0], trypoint) + distanc(kmeans[key][1], trypoint))/2
+        keywithminvalue = min(newdict, key=newdict.get)
+        means_toplot[keywithminvalue].append(trypoint)
+    plot_kmeans(means_toplot)
+
+
 def main():
     from main import readfile
     data = readfile('../data.txt')
     nearest_neighbour(data, 3)
+    knearest_neighbour(data, 3)
 
 
 if __name__ == '__main__':
