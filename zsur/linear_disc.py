d563cbecd7f05127098977656ed6802e4f0fccc2
mareklovci <mareklovci@gmail.com>
2018-03-22 13:50:50 +0100

    Rossenblatt
    Implementing Rossenblatt algorithm

--- zsur/linear_disc.py
+++ zsur/linear_disc.py
@@ -7,9 +7,38 @@
 * upravena metoda konstantnich prirustku
 """
 
+from zsur.genpoints import generate_points
+from zsur.readfile import readfile
+from zsur.kmeans import kmeans
+import numpy as np
+
+
+def rossenblatt(data, classes):
+    data = kmeans(data, classes)
+    lines = dict().fromkeys(data, [1, 1, 1])  # vyhrazeni prostoru pro koeficienty primek
+    for key, val in data.items():
+        cont = True
+        while cont:
+            cont = False
+            for k, v in data.items():
+                q = 1 if key == k else -1
+                for item in v:
+                    x = np.array((1, item[0], item[1]))
+                    if x.dot(lines[key]) * q < 0:
+                        lines[key] = np.transpose(lines[key]) + (x * q)
+                        cont = True
+    lines = {k: tuple(v) for k, v in lines.items()}
+    return lines
+
+
+def plot_ross(data):
+    pass
+
 
 def main():
-    pass
+    data = readfile('../data.txt')
+    ross = rossenblatt(data, 3)
+    print(ross)
 
 
 if __name__ == '__main__':
