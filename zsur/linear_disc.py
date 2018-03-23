af239e2e489ed95deb2cd5768a5c2ec2f80713b1
mareklovci <mareklovci@gmail.com>
2018-03-23 22:12:02 +0100

    Next part of Rossenblatt

--- zsur/linear_disc.py
+++ zsur/linear_disc.py
@@ -9,13 +9,12 @@
 
 from zsur.genpoints import generate_points
 from zsur.readfile import readfile
-from zsur.kmeans import kmeans
+from zsur.kmeans import kmeans, plot_kmeans
 import numpy as np
 
 
-def rossenblatt(data, classes):
-    data = kmeans(data, classes)
-    lines = dict().fromkeys(data, [1, 1, 1])  # vyhrazeni prostoru pro koeficienty primek
+def get_lines_ross(data: dict):
+    lines = dict.fromkeys(data, [1, 1, 1])  # vyhrazeni prostoru pro koeficienty primek
     for key, val in data.items():
         cont = True
         while cont:
@@ -31,14 +30,37 @@ def rossenblatt(data, classes):
     return lines
 
 
-def plot_ross(data):
-    pass
+def classify_ross(lines, trypoints):
+    classified = dict.fromkeys(lines)
+    for point in trypoints:
+        sides = dict.fromkeys(lines)
+        for key, val in lines.items():
+            sides[key] = val[0] * point[0] + val[1] * point[1] + val[2]
+        keys = [k for k, v in sides.items() if v > 1]
+        if len(keys) > 1:
+            continue
+        else:
+            k = keys[0]
+            if classified[k] is None:
+                classified[k] = [point]
+            else:
+                classified[k].append(point)
+    return classified
+
+
+def rossenblatt(data, classes, space_size=(-20, 20), step=1):
+    data = kmeans(data, classes)
+    lines = get_lines_ross(data)
+    trypoints = generate_points(space_size[0], space_size[1], step)
+    classified = classify_ross(lines, trypoints)
+    data = data.update(classified)
+    return data
 
 
 def main():
     data = readfile('../data.txt')
     ross = rossenblatt(data, 3)
-    print(ross)
+    plot_kmeans(ross)
 
 
 if __name__ == '__main__':
